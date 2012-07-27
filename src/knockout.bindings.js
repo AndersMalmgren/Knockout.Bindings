ba85d2716025fd4eac99aff1ed8d25628a137ac5
AndersMalmgren <anders.malmgren@agero.se>
2012-07-27 14:30:03 +0200

    Changed from my string based datepicker to RP Niemeyer's thats using real Dates

--- src/knockout.bindings.js
+++ src/knockout.bindings.js
@@ -39,16 +39,37 @@
     ko.virtualElements.allowedBindings.message = true;
 
     ko.bindingHandlers.datepicker = {
-        init: function (element, valueAccessor) {
-            var options = ko.utils.extend(ko.bindingHandlers.datepicker.defaultOptions, ko.utils.unwrapObservable(valueAccessor()) || {});
+        init: function (element, valueAccessor, allBindingsAccessor) {
+            //initialize datepicker with some optional options
+            var options = allBindingsAccessor().datepickerOptions || {};
+            $(element).datepicker(options);
 
+            //handle the field changing
+            ko.utils.registerEventHandler(element, "change", function () {
+                var observable = valueAccessor();
+                observable($(element).datepicker("getDate"));
+            });
+
+            //handle disposal (if KO removes by the template binding)
             ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
                 $(element).datepicker("destroy");
             });
 
-            $(element).datepicker(options);
         },
-        defaultOptions: {}
+        update: function (element, valueAccessor) {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+
+            //handle date data coming via json from Microsoft
+            if (String(value).indexOf('/Date(') == 0) {
+                value = new Date(parseInt(value.replace(/\/Date\((.*?)\)\//gi, "$1")));
+            }
+
+            current = $(element).datepicker("getDate");
+
+            if (value - current !== 0) {
+                $(element).datepicker("setDate", value);
+            }
+        }
     };
 
     ko.bindingHandlers.button = {
