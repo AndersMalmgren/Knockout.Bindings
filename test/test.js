007ca74883357bbaa5f9663d6c7ba2548dfd8c33
AndersMalmgren <anders.malmgren@agero.se>
2012-08-23 11:51:33 +0200

    Added more tests

--- test/test.js
+++ test/test.js
@@ -32,7 +32,7 @@
         equal(text, actualText, "It should show a splash message with correct text");
 
         setTimeout(function () {
-            equal(0, $(splashQuery).length, "It should hide and remove splash after a second"); //TODO: make splash time a bindable option
+            equal($(splashQuery).length, 0, "It should hide and remove splash after a second"); //TODO: make splash time a bindable option
             start();
         }, 50)
     })
@@ -40,7 +40,7 @@
     test("When updating message observable with a confirm message", function () {
         var text = "Test";
         alert = function (value) {
-            equal(text, value, "It should show a alert message with correct text");
+            equal(value, text, "It should show a alert message with correct text");
         };
         setupMessageTest({ alert: text });
     });
@@ -50,13 +50,13 @@
         var confirmResult = "Test";
 
         confirm = function (value) {
-            equal(text, value, "It should show a confirm message with correct text");
+            equal(value, text, "It should show a confirm message with correct text");
             return confirmResult;
         };
 
         var args = { confirm: text };
         setupMessageTest(args);
-        equal(confirmResult, args.result);
+        equal(args.result, confirmResult);
     });
 
     module("Datepicker binding");
@@ -73,7 +73,7 @@
         var date = new Date(2007);
         datePickerTest(date, function (input) {
             var actualDate = input.val();
-            equal(date.year, $(input).datepicker("getDate").year, "It should set the correct date");
+            equal($(input).datepicker("getDate").year, date.year, "It should set the correct date");
         });
     });
 
@@ -83,7 +83,7 @@
             var date = new Date(2008);
             $(input).datepicker("setDate", date);
 
-            equal(date.year, observable().year, "It should update observable with correct date");
+            equal(observable().year, date.year, "It should update observable with correct date");
         });
     });
 
@@ -95,7 +95,7 @@
         $.fn.datepicker = function (opt) {
             if (typeof (opt) == "string") return;
 
-            equal(options, opt, "It should use those options for the datepicker");
+            equal(opt, options, "It should use those options for the datepicker");
         };
 
         datePickerTest(date, function (input, observable) {
@@ -107,42 +107,151 @@
 
     module("Button binding");
 
+    var buttonTest = function (opt, assert, bindings) {
+        ko.test("button", ko.utils.extend({ button: opt }, bindings), function (button) {
+            assert(button);
+        });
+    };
+
     test("When using the button binding with a icon option", function () {
-        ko.test("button", { button: { icon: "test-icon", label: "Test"} }, function (button) {
-            equal(1, button.find(".ui-button-icon-primary").length, "It should have added the icon");
+        buttonTest({ icon: "test-icon", label: "Test" }, function (button) {
+            equal(button.find(".ui-button-icon-primary").length, 1, "It should have added the icon");
         });
     });
 
     test("When using the button binding with observable label", function () {
-        var actualText = "Test"
-        var label = ko.observable(actualText);
-        ko.test("button", { button: { icon: "test-icon", label: label} }, function (button) {
+        var expectedText = "Test"
+        var label = ko.observable(expectedText);
+        buttonTest({ label: label }, function (button) {
             var getButtonText = function () {
                 return button.find(".ui-button-text").html();
             };
-            equal(actualText, getButtonText(), "It should have a initial text set");
+            equal(getButtonText(), expectedText, "It should have a initial text set");
 
             actualText = "NewValue";
-            label(actualText);
-            equal(actualText, getButtonText(), "It should update button label with correct text");
+            label(expectedText);
+            equal(getButtonText(), expectedText, "It should update button label with correct text");
         });
+    });
+
+    test("When disabling the button from click handler", function () {
+        var enabled = ko.observable(true);
+        var click = function () {
+            enabled(false);
+        };
+
+        buttonTest({ icon: "test-icon", label: "Test" }, function (button) {
+            button.focus();
+            button.click();
+            equal(button.is(".ui-state-focus"), false, "It should not appear active");
+        }, { click: click, enable: enabled });
     });
 
     module("Dialog binding");
 
     test("When using a dialog binding", function () {
-        var actualTitle = "Test";
-        ko.test("button", { dialog: { title: actualTitle} }, function (dialog) {
-            equal(actualTitle, dialog.data("dialog").options.title, "It should configure dialog with correct options");
+        var expectedTitle = "Test";
+        ko.test("button", { dialog: { title: expectedTitle} }, function (dialog) {
+            equal(dialog.data("dialog").options.title, expectedTitle, "It should configure dialog with correct options");
         });
     });
 
     test("When using a dialog binding with a openDialog binding", function () {
         var open = ko.observable(false);
         ko.test("button", { dialog: { autoOpen: false }, openDialog: open }, function (dialog) {
-            equal(true, dialog.is(":hidden"), "It should be closed from the start");
+            equal(dialog.is(":hidden"), true, "It should be closed from the start");
             open(true);
-            equal(false, dialog.is(":hidden"), "It should listen on observable and open dialog");
+            equal(dialog.is(":hidden"), false, "It should listen on observable and open dialog");
+        });
+    });
+
+    module("Label binding");
+
+    var labelTest = function (opt, assert) {
+        ko.test("input type='checkbox'", { label: opt }, function (checkbox) {
+            var label = checkbox.siblings("label");
+            assert(checkbox, label);
+            label.remove();
+        });
+    };
+
+    test("When using a label binding", function () {
+        var text = "Test";
+        var checkboxId = "label-0";
+        labelTest({ title: text, caption: text }, function (checkbox, label) {
+            equal(label.html(), text, "It should have correct caption");
+            equal(label.attr("title"), text, "It should have correct title");
+            equal(checkbox.attr("id"), checkboxId, "It should have correct id");
+        });
+    });
+
+    test("When using a label binding and click on the label", function () {
+        labelTest({}, function (checkbox, label) {
+            label.click();
+            equal(checkbox.is(":checked"), true);
+        });
+    });
+
+    module("Tabs Binding");
+
+    var tabsTest = function (opt, assert) {
+        var template = $("<script id='tmpl' type='text/html'></script>").appendTo("body");
+
+        var tabs = ko.observableArray([new ko.TabViewModel(1, "Tab1", {}, "tmpl"), new ko.TabViewModel(2, "Tab2", {}, "tmpl")])
+        ko.test("div", { tabs: tabs, tabsOptions: opt }, function (element) {
+            var addTab = function () {
+                var id = tabs()[tabs().length - 1].id() + 1;
+                tabs.push(new ko.TabViewModel(id, "Tab" + id, {}, "tmpl"));
+            };
+            assert(element, tabs(), addTab);
+            template.remove();
+        });
+    };
+
+    test("When using a tab binding", function () {
+        tabsTest({}, function (element) {
+            var query = element.find("ul li")
+            equal(query.length, 2, "It should reflect number of tabs");
+            equal(query.find("a:first").html(), "Tab1", "It should have correct tab title");
+        });
+    });
+
+    test("When using a tab binding and setting selectedTab", function () {
+        var selectedTab = ko.observable();
+        tabsTest({ selectedTab: selectedTab }, function (element, tabs) {
+            selectedTab(tabs[1].model());
+            equal(element.find("ul li.ui-tabs-selected").length, 1, "It should have selected tab2");
+        });
+    });
+
+    test("When using a tab binding and selecting tab 2", function () {
+        var selectedTab = ko.observable();
+        tabsTest({ selectedTab: selectedTab }, function (element, tabs) {
+            element.find("ul li:last a").click()
+            equal(selectedTab(), tabs[1].model(), "It should have updated selectedTab observable correctly");
+        });
+    });
+
+    test("When binding the enable option to false", function () {
+        var enabled = ko.observable(true);
+        tabsTest({ enable: enabled, selectedTab: ko.observable(null) }, function (element, tabs) {
+            enabled(false);
+            equal(element.find("ul li.ui-state-disabled").length, 2, "It should disable all tabs");
+        });
+    });
+
+    test("When binding the enable option for a tab to false", function () {
+        tabsTest({ selectedTab: ko.observable(null) }, function (element, tabs) {
+            tabs[1].enable(false);
+            equal(element.find("ul li.ui-state-disabled").length, 1, "It should disable last tab");
+        });
+    });
+
+    test("When adding a tab", function () {
+        tabsTest({ selectedTab: ko.observable(null) }, function (element, tabs, addTab) {
+            addTab();
+            ok(element.find("ul li:last").attr("class") != null, "It should add jQuery tab stuff to button");
+            equal(element.find("div.ui-tabs-panel").length , 3, "It should add jQuery tab stuff to the tab itself");
         });
     });
 })();
