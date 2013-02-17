e97f95d8551a014999a7774ea52fdd5f84426095
Anders Malmgren <anders.malmgren@agero.se>
2013-02-17 14:03:46 +0100

    Missed to fix Demo after fix ce65510adbff551387afe4d2539194b364b5c2f8

--- demo.js
+++ demo.js
@@ -15,11 +15,7 @@ ViewModel = function () {
 
     this.selectedTabModel = ko.observable();
     this.tabsEnabled = ko.observable(true);
-
     this.cancelTabSelect = false;
-    this.onTabChanging = function (args) {
-        args.cancel = this.cancelTabSelect;
-    } .bind(this);
 
     this.optionItems = ko.observableArray([{ text: "Test1" }, { text: "Test2"}]);
     this.selectedOption = ko.observable({ text: "Test1" });
@@ -63,6 +59,9 @@ ViewModel.prototype = {
     },
     collapse: function () {
         this.selectedTabModel(null);
+    },
+    onTabChanging: function (args) {
+        args.cancel = this.cancelTabSelect;
     }
 };
 
